c3e41b18109a02851be816b9ea59dd193f25b1f1
Shinigami92 <chrissi92@hotmail.de>
2021-11-17 16:29:07 +0100

    Add test

---
+++ tests/ssg-mode.test.ts
@@ -0,0 +1,52 @@
+/**
+ * @jest-environment node
+ */
+
+import type { App } from 'vue';
+import VueGtm, { createGtm, useGtm } from '../src/index';
+import { createAppWithComponent } from './vue-helper';
+
+describe('SSG Mode', () => {
+  test('should expose Vue plugin', () => {
+    expect(VueGtm).toBeDefined();
+    expect(VueGtm.install).toBeDefined();
+    expect(VueGtm.install).toBeInstanceOf(Function);
+  });
+
+  test('should throw Error if GTM-ID is invalid', () => {
+    const validGtmId: string = 'GTM-X';
+    const invalidGtmIds: string[] = ['GTM-x', 'a', 'gtm-a', 'Error: ', 'Error'];
+    const fakeVueInstance: App = null as unknown as App;
+    for (const invalidGtmId of invalidGtmIds) {
+      const expectedErrorMessage: string = `GTM-ID '${invalidGtmId}' is not valid`;
+      expect(() => {
+        VueGtm.install?.(fakeVueInstance, { id: invalidGtmId });
+      }).toThrowError(new Error(expectedErrorMessage));
+      expect(() => {
+        VueGtm.install?.(fakeVueInstance, { id: [invalidGtmId] });
+      }).toThrowError(new Error(expectedErrorMessage));
+      expect(() => {
+        VueGtm.install?.(fakeVueInstance, { id: [validGtmId, invalidGtmId] });
+      }).toThrowError(new Error(expectedErrorMessage));
+      expect(() => {
+        VueGtm.install?.(fakeVueInstance, { id: [{ id: invalidGtmId }] });
+      }).toThrowError(new Error(expectedErrorMessage));
+      expect(() => {
+        VueGtm.install?.(fakeVueInstance, { id: [{ id: validGtmId }, { id: invalidGtmId }] });
+      }).toThrowError(new Error(expectedErrorMessage));
+    }
+  });
+
+  test('should expose useGtm function', () => {
+    expect(useGtm).toBeInstanceOf(Function);
+
+    // If the plugin was not used, it returns undefined
+    expect(useGtm()).toBeUndefined();
+
+    const { app } = createAppWithComponent();
+    app.use(createGtm({ id: 'GTM-DEMO' }));
+
+    expect(useGtm()).toBeDefined();
+    expect(useGtm()).toStrictEqual(app.config.globalProperties.$gtm);
+  });
+});
